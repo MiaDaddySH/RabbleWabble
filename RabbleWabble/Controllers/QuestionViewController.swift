ebffc42daa77f2a86e2bb469c0530b27fa21e337
Martin <yuangang.sheng@itc-engineering.com>
2021-05-18 21:30:27 +0200

    Initial Commit

---
+++ RabbleWabble/Controllers/QuestionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RabbleWabble
+//
+//  Created by Yuangang Sheng on 18.05.21.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
